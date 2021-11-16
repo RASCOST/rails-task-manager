cda34bb68433b8cf41cfa7d4dc8d324af541d674
Ricardo Costa <ra.silva.dcosta@gmail.com>
2021-11-16 12:01:23 +0100

    created actions new, create and the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+
   get 'task/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
 end
